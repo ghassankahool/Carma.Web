33d3e7e424ee85a85437b0a557c4075efdda800e
M. Salah <eng.msalah.abdullah@gmail.com>
2018-05-14 19:16:42 +0400

    Add project files.

---
+++ Carma.Web/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Carma.Web.Startup))]
+namespace Carma.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
